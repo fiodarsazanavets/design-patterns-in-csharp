bd20a890c96b95c7af6a15353dc1283cc0f1151a
Fiodar Sazanavets <33926695+fiodarsazanavets@users.noreply.github.com>
2022-04-08 20:02:34 +0100

    Apply .NET 6 syntax

--- Behavioral_Patterns/Command/Command-Demo/DataReceiver.cs
+++ Behavioral_Patterns/Command/Command-Demo/DataReceiver.cs
@@ -1,27 +1,26 @@
-﻿namespace Command_Demo
+﻿namespace Command_Demo;
+
+internal class DataReceiver
 {
-    internal class DataReceiver
+    private readonly Dictionary<string,string> data;
+
+    public DataReceiver()
     {
-        private readonly Dictionary<string,string> data;
+        data = new Dictionary<string, string>();
+    }
 
-        public DataReceiver()
-        {
-            data = new Dictionary<string, string>();
-        }
+    public void Upsert(string key, string value)
+    {
+        data[key] = value;
+        Console.WriteLine($"Upserted: {key} - {value}.");
+    }
 
-        public void Upsert(string key, string value)
+    public void Delete(string key)
+    {
+        if (data.ContainsKey(key))
         {
-            data[key] = value;
-            Console.WriteLine($"Upserted: {key} - {value}.");
-        }
-
-        public void Delete(string key)
-        {
-            if (data.ContainsKey(key))
-            {
-                data.Remove(key);
-                Console.WriteLine($"Removed: {key}.");
-            }
+            data.Remove(key);
+            Console.WriteLine($"Removed: {key}.");
         }
     }
 }
