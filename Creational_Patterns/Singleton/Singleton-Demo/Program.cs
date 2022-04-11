ca1aea30f58b2e4504a471eb5b2d5152fb76795a
Fiodar Sazanavets <33926695+fiodarsazanavets@users.noreply.github.com>
2022-04-11 19:23:12 +0100

    Fix a typo

--- Creational_Patterns/Singleton/Singleton-Demo/Program.cs
+++ Creational_Patterns/Singleton/Singleton-Demo/Program.cs
@@ -6,6 +6,6 @@ Console.WriteLine($"Data of object 1: {object1.Data}");
 var object2 = SingletonObject.GetInstance();
 Console.WriteLine($"Data of object 2: {object2.Data}");
 
-Console.WriteLine($"Are object equal? {object.Equals(object1, object2)}");
+Console.WriteLine($"Are objects equal? {object.Equals(object1, object2)}");
 
 Console.ReadKey();
